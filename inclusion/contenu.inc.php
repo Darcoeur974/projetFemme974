ef5742dba88c292c83f8a7ea79009c70de0bf13d
Darcoeur974 <darcoeur136dark@gmail.com>
2019-09-30 15:56:47 +0400

    etape inclusions

---
+++ inclusion/contenu.inc.php
@@ -0,0 +1,22 @@
+<body>
+<div class="dark">
+    <!--EXERCICE VARIABLE-->
+<button>
+    <a href='php/exerciceVar.php'>Vers exercice sur les Variables</a>
+</button>
+
+<!--EXERCICE CONDITIONS-->
+<button>
+    <a href='php/exerciceCon.php'>Vers exercice sur les Conditions</a>
+</button>
+
+<!--EXERCICE BOUCLE-->
+<button>
+    <a href='php/exerciceBou.php'>Vers exercice sur les Boucles</a>
+</button>
+
+<!--EXERCICE FONCTION-->
+<button>
+    <a href='php/exerciceFon.php'>Vers exercice sur les Fonctions</a>
+</button>
+</div>
--- index.php
+++ index.php
@@ -1,34 +1,6 @@
 <?php
-echo 
-'<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>';
-
-      //EXERCICE VARIABLE
-echo "<button>
-        <a href='php/exerciceVar.php'>Vers exercice sur les Variables</a>
-      </button>";
-
-      //EXERCICE CONDITIONS
-echo "<button>
-        <a href='php/exerciceCon.php'>Vers exercice sur les Conditions</a>
-      </button>";
-
-      //EXERCICE BOUCLE
-echo "<button>
-        <a href='php/exerciceBou.php'>Vers exercice sur les Boucles</a>
-      </button>";
-
-      //EXERCICE Fonctions
-echo "<button>
-        <a href='php/exerciceFon.php'>Vers exercice sur les Fonctions</a>
-      </button>";
+include('inclusion/header.inc.php');
+include('inclusion/contenu.inc.php');
+?>
 
 
