986324c2c3db722eb2657977270a0b7e35139616
Darcoeur974 <darcoeur136dark@gmail.com>
2019-09-30 12:38:50 +0400

    etape fonction

---
+++ php/exerciceBou.php
@@ -0,0 +1,135 @@
+<?php
+echo 
+'<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>';
+
+//Exercice 1
+//Créer une variable et l'initialiser à 0.
+//Tant que cette variable n'atteint pas 10, il faut :
+//l'afficher
+//l'incrementer
+echo '<h1>Exercice sur les Boucles</h1>';
+echo '<section><h2>Exercice 1</h2>';
+
+$variable = 0;
+while ($variable < 10) {
+    echo $variable . '<br />';
+    $variable++;
+}
+
+echo '</section>';
+
+//Exercice 2
+//Créer deux variables. Initialiser la première à 0 et la deuxième avec un nombre compris en 1 et 100.
+//Tant que la première variable n'est pas supérieure à 20 :
+//multiplier la première variable avec la deuxième
+//afficher le résultat
+//incrementer la première variable
+echo '<section><h2>Exercice 2</h2>';
+
+$premiereVariable = 0;
+$secondeVariable = 50;
+while ($premiereVariable <= 20) {
+    echo 'variable 1 : ' . $premiereVariable . '<br />';
+    echo 'calcul : ' . $premiereVariable * $secondeVariable . '<br />';
+    $premiereVariable++;
+}
+
+echo '</section>';
+
+//Exercice 3
+//Créer deux variables. Initialiser la première à 100 et la deuxième avec un nombre compris en 1 et 100.
+//Tant que la première variable n'est pas inférieure ou égale à 10 :
+//multiplier la première variable avec la deuxième
+//afficher le résultat
+//décrémenter la première variable
+echo '<section><h2>Exercice 3</h2>';
+
+$premiereVariable = 100;
+$secondeVariable = 50;
+while ($premiereVariable >= 10) {
+    echo 'variable 1 : ' . $premiereVariable . '<br />';
+    echo 'calcul : ' . $premiereVariable * $secondeVariable . '<br />';
+    $premiereVariable--;
+}
+
+echo '</section>';
+
+//Exercice 4
+//Créer une variable et l'initialiser à 1.
+//Tant que cette variable n'atteint pas 10, il faut :
+//l'afficher
+//l'incrementer de la moitié de sa valeur
+echo '<section><h2>Exercice 4</h2>';
+
+$variable = 1;
+while ($variable <= 10) {
+    echo 'variable : ' . $variable . '<br />';
+    $variable = $variable + ($variable/2);
+}
+
+echo '</section>';
+
+//Exercice 5
+//En allant de 1 à 15 avec un pas de 1, afficher le message On y arrive presque.
+echo '<section><h2>Exercice 5</h2>';
+
+$variable = 1;
+while ($variable <= 15) {
+    $variable++;
+    if ($variable <= 15) {
+        echo 'On y arrive presque! <br />';
+    }
+}
+
+echo '</section>';
+
+//Exercice 6
+//En allant de 20 à 0 avec un pas de 1, afficher le message C'est presque bon.
+echo '<section><h2>Exercice 6</h2>';
+
+$variable = 20;
+while ($variable > 0) {
+    $variable--;
+    if ($variable > 0) {
+        echo 'C\'est presque bon <br />';
+    }
+}
+
+echo '</section>';
+
+//Exercice 7
+//En allant de 1 à 100 avec un pas de 15, afficher le message On tient le bon bout.
+echo '<section><h2>Exercice 7</h2>';
+
+$variable = 1;
+while ($variable < 100) {
+    $variable+=15;
+    if ($variable < 100) {
+        echo 'On tient le bon bout <br />';
+    }
+}
+
+echo '</section>';
+
+//Exercice 8
+//En allant de 200 à 0 avec un pas de 12, afficher le message Enfin !!!!.
+echo '<section><h2>Exercice 8</h2>';
+
+$variable = 200;
+while ($variable > 0) {
+    $variable-=12;
+    if ($variable > 0) {
+        echo 'Enfin !!!! <br />';
+    }
+}
+
+echo '</section>';
--- php/exerciceCon.php
+++ php/exerciceCon.php
@@ -1,4 +1,15 @@
 <?php
+echo 
+'<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>';
 
 //EXERCICE Conditions
 //Exercice 1
@@ -21,7 +32,7 @@ echo '</section>';
 //Créer une variable isEasy de type booléan et l'initialiser avec une valeur.
 //Afficher C'est facile!! si c'est vrai. Dans le cas contraire afficher C'est difficile !!!.
 //Bonus : L'écrire de deux manières différentes.
-echo '<section><h2>Exercice 1</h2>';
+echo '<section><h2>Exercice 2</h2>';
 
 $isEasy = true;
 
@@ -43,7 +54,7 @@ echo '</section>';
 //Vous êtes une femme et vous êtes majeur
 //Vous êtes une femme et vous êtes mineur
 //Gérer tous les cas.
-echo '<section><h2>Exercice 1</h2>';
+echo '<section><h2>Exercice 3</h2>';
 
 $age = 16;
 $gender = "Femme";
@@ -57,5 +68,115 @@ if ($gender == "Homme" && $age >= 18) {
 } else {
     echo "Vous êtes une femme et vous êtes mineur";
 }
+
+echo '</section>';
+
+//Exercice 4
+//L'échelle de Richter est un outil de mesure qui permet de définir la magnitude de moment d'un tremblement de terre. Cette échelle va de 1 à 9.
+//Créer une variable magnitude. Selon la valeur de magnitude, afficher la phrase correspondante.
+//Gérer tous les cas.
+//Utilser autre chose que des if else
+
+echo '<section><h2>Exercice 4</h2>';
+
+$magnitude = 4;
+
+switch ($magnitude) {
+    case 1:
+        echo 'Micro-séisme impossible à ressentir.';
+    break;
+
+    case 2:
+        echo 'Micro-séisme impossible à ressentir mais enregistrable par les sismomètres.';
+    break;
+
+    case 3:
+        echo 'Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti.';
+    break;
+
+    case 4:
+        echo 'Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats.';
+    break;
+
+    case 5:
+        echo 'Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats.';
+    break;
+
+    case 6:
+        echo 'Fort séisme capable d\'engendrer des destructions majeures sur une large distance (180 km) autour de l\'épicentre.';
+    break;
+
+    case 7:
+        echo 'Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance.';
+    break;
+
+    case 8:
+        echo 'Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres.';
+    break;
+
+    case 9:
+        echo 'Séisme capable de tout détruire sur une très vaste zone.';
+    break;
+}
+
+echo '</section>';
+
+//Exercice 5
+//Traduire ce code avec des if et des else :
+//<?php
+//echo ($gender != 'Homme') ? 'C\'est une développeuse !!!' : 'C\'est un développeur !!!';
+echo '<section><h2>Exercice 5</h2>';
+
+$gender = "Femme";
+if ($gender != "Homme") {
+    echo 'C\'est une développeuse !!!';
+} else {
+    echo 'C\'est un développeur !!!';
+};
+
+echo '</section>';
+
+//Exercice 6
+//Traduire ce code avec des if et des else :
+//<?php
+//echo ($age >= 18) ? 'Tu es majeur' : 'Tu n\'es pas majeur';
+echo '<section><h2>Exercice 6</h2>';
+
+$age = 16;
+if ($age >= 18) {
+    echo 'Tu es majeur';
+} else {
+    echo 'Tu n\'es pas majeur';
+};
+
+echo '</section>';
+
+//Exercice 7
+//Traduire ce code avec des if et des else :
+//<?php
+//echo ($isOk == false) ? 'c\'est pas bon !!!' : 'c\'est ok !!';
+echo '<section><h2>Exercice 7</h2>';
+
+$isOk = false;
+if ($isOk == false) {
+    echo 'c\'est pas bon !!!';
+} else {
+    echo 'c\'est ok !!';
+};
+
+echo '</section>';
+
+//Exercice 8
+//Traduire ce code avec des if et des else :
+//<?php
+//echo ($isOk) ? 'c'est ok !!' : 'c'est pas bon !!!';
+echo '<section><h2>Exercice 8</h2>';
+
+$isOk = true;
+if ($isOk) {
+    echo 'c\'est ok !!';
+} else {
+    echo 'c\'est pas bon !!!';
+};
 
 echo '</section>';
