16110025b5ad4bd45c79d49e05ead7cada07682a
darcoeur974 <oconnormorgan974@gmail.com>
2019-10-02 11:46:07 +0400

    Morgan : fin du formulaire (non style)

--- php/formulaire.inc.php
+++ php/formulaire.inc.php
@@ -1,10 +1,10 @@
-<form action="" class="formulaire">
+<form action="php/post_contact.php" method="POST" class="formulaire">
     <div class="row">
         <div class="col-2">
             <label for="emailinput">E-mail</label>
         </div>
         <div class="col-10">
-            <input type="email" id="emailinput" placeholder="payet.marielyse@gmail.com...">
+            <input type="email" name="email" id="emailinput" placeholder="payet.marielyse@gmail.com...">
         </div>
     </div>
     <div class="row">
@@ -12,13 +12,13 @@
             <label for="objetinput">Objet</label>
         </div>
         <div class="col-10">
-            <input type="text" id="objetinput" placeholder="objet de votre message...">
+            <input type="text" name="objet"  id="objetinput" placeholder="objet de votre message...">
         </div>
     </div>
     <div class="row">
         <textarea name="texte" placeholder="votre message..." rows="6"></textarea>
     </div>
     <div class="row">
-        <button class="btn btn-primary">Envoyer</button>
+        <button type="submit" class="btn btn-primary">Envoyer</button>
     </div>
 </form>
