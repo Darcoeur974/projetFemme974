765a0a5c2e149d2bb5d25d8f84866c1680f6e4a4
darcoeur974 <oconnormorgan974@gmail.com>
2019-10-02 09:26:23 +0400

    debut footer

--- php/footer.inc.php
+++ php/footer.inc.php
@@ -1,1 +1,51 @@
-<h3>FOOTER</h3>
+<footer>
+    <h3>FOOTER</h3>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <ul>
+                    <li><a href="">Menue #</a></li>
+                    <li><a href="">Menue #</a></li>
+                    <li><a href="">Menue #</a></li>
+                    <li><a href="">Menue #</a></li>
+                    <li><a href="">Menue #</a></li>
+                </ul>
+            </div>
+            <div class="col-6">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="https://via.placeholder.com/124x90.png" alt="">
+                    </div>
+                    <div class="col-3">
+                        <img src="https://via.placeholder.com/124x90.png" alt="">
+                    </div>
+                    <div class="col-3">
+                        <img src="https://via.placeholder.com/124x90.png" alt="">
+                    </div>
+                    <div class="col-3">
+                        <img src="https://via.placeholder.com/124x90.png" alt="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-3">
+                        <img src="https://via.placeholder.com/124x90.png" alt="">
+                </div>
+                    <div class="col-3">
+                        <img src="https://via.placeholder.com/124x90.png" alt="">
+                </div>
+                    <div class="col-3">
+                        <img src="https://via.placeholder.com/124x90.png" alt="">
+                </div>
+                    <div class="col-3">
+                        <img src="https://via.placeholder.com/124x90.png" alt="">
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="js/script.js"></script>
