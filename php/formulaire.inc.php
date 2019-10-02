a0a5263e8b20cd1b66c96397caad40ac73506e0f
darcoeur974 <oconnormorgan974@gmail.com>
2019-10-02 11:12:45 +0400

    Morgan : modification formulaire

--- php/formulaire.inc.php
+++ php/formulaire.inc.php
@@ -1,24 +1,24 @@
 <form action="" class="formulaire">
     <div class="row">
         <div class="col-2">
-            <label for="email">E-mail</label>
+            <label for="emailinput">E-mail</label>
         </div>
         <div class="col-10">
-            <input type="email" placeholder="payet.marielyse@gmail.com...">
+            <input type="email" id="emailinput" placeholder="payet.marielyse@gmail.com...">
         </div>
     </div>
     <div class="row">
         <div class="col-2">
-            <label for="objet">Objet</label>
+            <label for="objetinput">Objet</label>
         </div>
         <div class="col-10">
-            <input type="text" placeholder="objet de votre message...">
+            <input type="text" id="objetinput" placeholder="objet de votre message...">
         </div>
     </div>
     <div class="row">
         <textarea name="texte" placeholder="votre message..." rows="6"></textarea>
     </div>
     <div class="row">
-        <button>Envoyer</button>
+        <button class="btn btn-primary">Envoyer</button>
     </div>
 </form>
