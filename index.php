d43f38a283780d7ff91b52907568e8871ec3297e
Darcoeur974 <darcoeur136dark@gmail.com>
2019-10-07 14:58:06 +0400

    MORGAN probleme navbar

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-include('php/header.inc.php');?>
-<?php include('php/navBar.inc.php');?>
+include('php/header.inc.php');
+include('php/navBar.inc.php');?>
+
 <div id="DIVID">
   <?php
   include('php/contenu.inc.php');
